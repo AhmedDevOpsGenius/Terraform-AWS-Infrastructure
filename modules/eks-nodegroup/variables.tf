5d0f409cbdfe46b82e47c8d88da6dac79c87c8e7
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-03-31 02:21:20 +0500

    Update variables.tf

--- modules/eks-nodegroup/variables.tf
+++ modules/eks-nodegroup/variables.tf
@@ -1,36 +1,49 @@
 variable "region" {
-  description = "AWS region"
+  description = "The AWS region"
+  type        = string
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "The name for the EKS cluster"
+  type        = string
 }
 
 variable "cluster_version" {
-  description = "Kubernetes version for the EKS cluster"
+  description = "The Kubernetes version for the EKS cluster"
+  type        = string
 }
 
-variable "subnets" {
-  description = "List of subnets for the EKS cluster"
+variable "cluster_role_arn" {
+  description = "The ARN of the IAM role that provides permissions for the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs for the EKS cluster and node groups"
   type        = list(string)
 }
 
-variable "vpc_id" {
-  description = "ID of the VPC"
+variable "node_group_name" {
+  description = "The name for the EKS node group"
+  type        = string
 }
 
-variable "worker_instance_type" {
-  description = "Instance type for worker nodes"
+variable "instance_types" {
+  description = "A list of instance types for the EKS worker nodes"
+  type        = list(string)
 }
 
-variable "worker_desired_capacity" {
-  description = "Desired number of worker nodes"
+variable "desired_size" {
+  description = "The desired number of nodes in the EKS node group"
+  type        = number
 }
 
-variable "worker_max_capacity" {
-  description = "Maximum number of worker nodes"
+variable "min_size" {
+  description = "The minimum number of nodes in the EKS node group"
+  type        = number
 }
 
-variable "worker_min_capacity" {
-  description = "Minimum number of worker nodes"
+variable "max_size" {
+  description = "The maximum number of nodes in the EKS node group"
+  type        = number
 }
